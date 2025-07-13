9f16762262ef748b4d6fabd99a3336be64801395
Timo Neske <neske.timo93@gmail.com>
2025-07-13 22:46:13 +0200

    added java doc

--- src/main/java/com/quickdomain/api/gpt/GptStrategyFactory.java
+++ src/main/java/com/quickdomain/api/gpt/GptStrategyFactory.java
@@ -1,5 +1,16 @@
 package com.quickdomain.api.gpt;
 
+/**
+ * Factory interface for creating instances of {@link GptStrategy}.
+ * This allows for the creation of different strategies based on the provider.
+ *
+ * @since 1.0.0
+ * @author Timo Neske
+ */
 public interface GptStrategyFactory {
+
+    /**
+     * @return a new instance of {@link GptStrategy} based on the provider.
+     */
     GptStrategy create();
 }
