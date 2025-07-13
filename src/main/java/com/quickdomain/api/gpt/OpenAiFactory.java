1b5869daff95447f2a1c0a327e8462377cb3584d
Timo Neske <neske.timo93@gmail.com>
2025-07-13 22:47:46 +0200

    added java doc

--- src/main/java/com/quickdomain/api/gpt/OpenAiFactory.java
+++ src/main/java/com/quickdomain/api/gpt/OpenAiFactory.java
@@ -1,6 +1,16 @@
 package com.quickdomain.api.gpt;
 
+/**
+ * Factory for creating instances of {@link OpenAiStrategy}.
+ *
+ * @since 1.0.0
+ * @author Timo Neske
+ */
 public class OpenAiFactory implements GptStrategyFactory{
+
+    /**
+     * @see GptStrategyFactory#create()
+     */
     @Override
     public GptStrategy create() {
         return new OpenAiStrategy();
