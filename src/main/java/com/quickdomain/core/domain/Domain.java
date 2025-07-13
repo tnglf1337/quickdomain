f45e6ddca15734ea5f6d8c1035495e650c14d9cb
Timo Neske <neske.timo93@gmail.com>
2025-07-13 22:54:43 +0200

    added java doc

--- src/main/java/com/quickdomain/core/domain/Domain.java
+++ src/main/java/com/quickdomain/core/domain/Domain.java
@@ -1,7 +1,5 @@
 package com.quickdomain.core.domain;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -16,19 +14,22 @@ public interface Domain<T> {
     /**
      * This method implements the logic to generate the entities for the domain.
      * @return List<T> is the list with <code>n</code> generated entities.
-     * @throws NoSuchMethodException
-     * @throws InvocationTargetException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws IOException
-     * @throws InterruptedException
      */
-    List<T> generate() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, InterruptedException;
+    List<T> generate();
 
+    /**
+     * This method implements the logic to generate the entities for the domain by a raw <code>String</code> content.
+     * @param content is the content to generate entities from. The string must be in format: <p><code>'fieldName1,val1,val2\nfieldName2,val1,val2'</code></p> fieldNames must be separated by '\n'.
+     * @return List<T> is the list with <code>n</code> generated entities.
+     */
+    List<T> generate(String content);
 
-    List<T> generate(String content) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
-
-    List<T> generate(Path filePath) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
+    /**
+     * This method implements the logic to generate the entities for the domain by a file path to an Csv file. More information at {@link com.quickdomain.util.CsvReader}
+     * @param filePath is the relative path to the csv file to read from.
+     * @return List<T> is the list with <code>n</code> generated entities.
+     */
+    List<T> generate(Path filePath);
 
     /**
      * Implementation has to validate class variable types of <code>T</code>.
